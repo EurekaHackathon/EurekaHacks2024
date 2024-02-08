4bd7316c90ba8dff89b1bb11999651a4200b36b8
_Leg3ndary <ben.zhou.2006@gmail.com>
2024-02-07 20:06:43 -0500

    Remove yohance

--- src/components/Team/Members.js
+++ src/components/Team/Members.js
@@ -11,7 +11,7 @@ export const members = [
     },
     {
         name: "Yohance Pawania",
-        position: "Director of Logistics / Web Development",
+        position: "Director of Logistics",
         image: "Team/Yohance.jpg",
     },
     {
