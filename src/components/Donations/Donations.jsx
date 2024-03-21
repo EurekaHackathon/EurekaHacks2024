05c7bbc6bc29c3e99f620f849e748dfbce1affee
OrioleNFinch <112213597+OrioleNFinch@users.noreply.github.com>
2024-03-21 14:35:35 -0400

    fix donation page

--- src/components/Donations/Donations.jsx
+++ src/components/Donations/Donations.jsx
@@ -1,10 +1,19 @@
 import styles from "@/components/Donations/Donations.module.css";
 import DonateImage from "@/../public/Sponsors/donation.jpg";
 import Image from "next/image";
+import Link from "next/link";
+import Logo from "@/../public/EurekaIcon2024.png";
 
 export default function Donations() {
     return (
         <>
+            <Link href="/" className={styles["navbar-box"]}>
+                <Image
+                    className={styles["navbar-icon"]}
+                    src={Logo}
+                    alt="Eureka! Icon"
+                />
+            </Link>
             <div className={styles["donate"]}>
                 <div className={styles["donate-text"]}>
                     <h2>Donation Steps</h2>
