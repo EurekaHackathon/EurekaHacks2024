d9a029b2e88ce3144bcc67873a4c1fd421cbc5e5
_Leg3ndary <ben.zhou.2006@gmail.com>
2024-04-19 16:31:26 -0400

    lint and move things around

--- src/components/TimeTable/TimeTable.jsx
+++ src/components/TimeTable/TimeTable.jsx
@@ -1,24 +1,33 @@
 import { Fragment } from "react";
 import styles from "./TimeTable.module.css";
 
-export default function TimeTable({ startTime, endTime, timeInc, columnNames, events }) {
+export default function TimeTable({
+    startTime,
+    endTime,
+    timeInc,
+    columnNames,
+    events,
+}) {
     var times = [startTime];
-    
+
     for (;;) {
         let prevTime = times.at(-1);
-        let nextTime = { hour: prevTime.hour, minute: prevTime.minute + timeInc };
-        
+        let nextTime = {
+            hour: prevTime.hour,
+            minute: prevTime.minute + timeInc,
+        };
+
         if (nextTime.minute >= 60) {
             nextTime.hour += Math.floor(nextTime.minute / 60);
             nextTime.minute %= 60;
         }
-        
+
         if (prevTime.hour >= endTime.hour && prevTime.minute >= endTime.minute)
             break;
 
         times.push(nextTime);
     }
-    
+
     return (
         <div className={styles.table}>
             {columnNames.map((name, index) => {
@@ -34,20 +43,31 @@ export default function TimeTable({ startTime, endTime, timeInc, columnNames, ev
 
                 return (
                     <Fragment key={index}>
-                        <div  className={styles.marker}>
-                            <p className={isHour ? styles.isHour : styles.isMinute}>
-                                {time.hour <= 12 ? time.hour : time.hour - 12}:{isHour ? "00" : time.minute}
+                        <div className={styles.marker}>
+                            <p
+                                className={
+                                    isHour ? styles.isHour : styles.isMinute
+                                }
+                            >
+                                {time.hour <= 12 ? time.hour : time.hour - 12}:
+                                {isHour ? "00" : time.minute}
                             </p>
                         </div>
-
                         <div className={styles.divider}></div>
                     </Fragment>
                 );
             })}
 
             {events.map((event, index) => {
-                let rowSpan = Math.floor(event.duration / timeInc * 2)
-                let rowStart = Math.floor(((event.start.hour - startTime.hour) * 60 + event.start.minute - startTime.minute) / timeInc * 2 + 2);
+                let rowSpan = Math.floor((event.duration / timeInc) * 2);
+                let rowStart = Math.floor(
+                    (((event.start.hour - startTime.hour) * 60 +
+                        event.start.minute -
+                        startTime.minute) /
+                        timeInc) *
+                        2 +
+                        2,
+                );
 
                 return (
                     <div
@@ -62,7 +82,7 @@ export default function TimeTable({ startTime, endTime, timeInc, columnNames, ev
                         <p>{event.name}</p>
                     </div>
                 );
-            })}  
+            })}
         </div>
     );
 }
