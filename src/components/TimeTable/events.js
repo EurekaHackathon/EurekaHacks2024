ca9471e508a46d44afba93988042499b2df642ea
Leg3ndary <ben.zhou.2006@gmail.com>
2024-04-26 08:46:49 -0400

    Add all events

--- src/components/TimeTable/events.js
+++ src/components/TimeTable/events.js
@@ -37,6 +37,7 @@ export const timeTableData = {
             },
             column: 2,
             duration: 15,
+            description: "Allocated time where participants connect with like-minded individuals to form teams to collaborate on a project for the event. Whether you're a developer, designer, or enthusiast, this is your chance to find a team to build something incredible together."
         },
         {
             name: "Idea Brainstorm",
@@ -47,6 +48,7 @@ export const timeTableData = {
             },
             column: 2,
             duration: 30,
+            description: "Join us for a scavenger hunt, where you explore the school to solve clues, complete challenges, and race against the clock. Gather your friends, sharpen your mind, and try to win!"
         },
         {
             name: "Git Essentials",
@@ -57,6 +59,7 @@ export const timeTableData = {
             },
             column: 2,
             duration: 15,
+            description: "Learn about the fundamentals of Git, the cornerstone of modern software development. Join us for a session where you'll learn basics, essential commands, and efficient collaboration practices."
         },
         {
             name: "Intro to Block Chain With Cryptochicks",
@@ -67,6 +70,7 @@ export const timeTableData = {
             },
             column: 3,
             duration: 60,
+            description: "Discover the revolutionary world of blockchain technology with CryptoChicks. Dive into the basics, explore its applications, and learn how to get started."
         },
         {
             name: "Smash Bros Tournament",
@@ -77,6 +81,7 @@ export const timeTableData = {
             },
             column: 4,
             duration: 60,
+            description: "Battle it out in the ultimate showdown of skill and strategy! Compete against fellow gamers in a Super Smash Bros tournament. Are you ready to claim the title of champion?"
         },
         {
             name: "Intro to Quantum Computing",
@@ -87,6 +92,7 @@ export const timeTableData = {
             },
             column: 3,
             duration: 60,
+            description: "Explore the next step of computing with an introductory session on quantum mechanics and its applications in computing. Join us to understand the mysteries of qubits, quantum algorithms, and the potential of this groundbreaking technology."
         },
         {
             name: "Mario Kart Tournament",
@@ -97,6 +103,7 @@ export const timeTableData = {
             },
             column: 4,
             duration: 60,
+            description: "Rev up your engines and prepare for a racing session with your peers.  Race to victory and claim the desired first place!"
         },
         {
             name: "Lunch",
@@ -117,6 +124,7 @@ export const timeTableData = {
             },
             column: 3,
             duration: 60,
+            description: "Explore the world of robotics with Orbit 3160: An innovative team pushing the boundaries of technology. Join us for a hands-on workshop where you’ll explore robotic principles."
         },
         {
             name: "Reach Trivia",
@@ -127,6 +135,7 @@ export const timeTableData = {
             },
             column: 4,
             duration: 60,
+            description: "Test your knowledge and quick thinking with our Reach team! Join us for a thrilling session filled with brain-teasing questions, friendly competition, and plenty of fun."
         },
         {
             name: "Ping Pong Tournament",
@@ -137,6 +146,7 @@ export const timeTableData = {
             },
             column: 4,
             duration: 45,
+            description: "Get ready to serve up some fun! Compete in our Ping Pong Tournament, where players of any level can showcase their skills, enjoy competition, and aim for the win!"
         },
         {
             name: "Lecture on AI",
@@ -147,6 +157,7 @@ export const timeTableData = {
             },
             column: 3,
             duration: 60,
+            description: "Delve into the world of Artificial Intelligence in our engaging lecture. Discover the principles, applications, and future trends shaping AI technology. Whether you're a novice or an expert, this session offers insights that will expand your understanding of this field."
         },
         {
             name: "City Guesser",
@@ -167,6 +178,7 @@ export const timeTableData = {
             },
             column: 4,
             duration: 60,
+            description: "Prepare to make your moves and outwit your opponents in our chess tournament! Whether you're a seasoned grandmaster or a beginner, join us for an hour of strategic battles and intense competition."
         },
         {
             name: "TypeRacer",
@@ -177,6 +189,7 @@ export const timeTableData = {
             },
             column: 4,
             duration: 30,
+            description: "Ready, set, type! Join our Typeracer tournament and put your typing speed and accuracy to the test. Compete against fellow typists to see who comes out on top!"
         },
         {
             name: "Karaoke/Lounge",
@@ -187,6 +200,7 @@ export const timeTableData = {
             },
             column: 2,
             duration: 120,
+            description: "Grab the mic and show your inner rockstar in our karaoke and lounge session. Sing your heart out to your favorite tunes, and join peers for duets. Let the music take center stage. "
         },
         {
             name: "Hot Wings Challenge",
@@ -197,6 +211,7 @@ export const timeTableData = {
             },
             column: 4,
             duration: 60,
+            description: "Dare to take on the heat in our Hot Wings Challenge! Test your tolerance with progressively spicy wings from WingsUp and see if you can handle the heat. Are you brave enough to conquer the challenge and earn your place among the spice champions?"
         },
         {
             name: "Dinner",
@@ -227,6 +242,7 @@ export const timeTableData = {
             },
             column: 6,
             duration: 30,
+            description: "Join us for a scavenger hunt, where you explore the school to solve clues, complete challenges, and race against the clock. Gather your friends, sharpen your mind, and try to win!"
         },
         {
             name: "Face & Banner Painting",
@@ -237,6 +253,7 @@ export const timeTableData = {
             },
             column: 6,
             duration: 30,
+            description: "Let our artists transform your face into a work of art."
         },
     ],
 };
